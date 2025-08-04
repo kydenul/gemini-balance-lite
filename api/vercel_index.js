b06918bbcccdca4ca454ed807175010497b6dc00
Kyden <kydenul@gmail.com>
2025-08-04 17:01:27 +0800

    Updates README and fixes proxy URL
    Updates the README to reflect the correct repository URL and removes outdated deployment instructions.
    
    Also, fixes the proxy URL in the main handler.

--- api/vercel_index.js
+++ api/vercel_index.js
@@ -1,7 +1,7 @@
 import { handleRequest } from "../src/handle_request.js";
 
 export const config = {
-  runtime: 'edge' //告诉 Vercel 这是 Edge Function
+  runtime: 'edge'
 };
 
 export default async function handler(req) {
